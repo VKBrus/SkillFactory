017b9965c49a9061e23ccf65bcdd7930819c8576
FNikols <fedorov_nikolai@mail.ru>
2021-08-27 21:56:32 +0400

    B7.4

---
+++ B7/yandex-cloud-terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "token" {
+  type    = string
+  default = "AQAAAAAiOJ0rAATuwRC_RJng_kWXjPcStsjBpvg"
+}
+
+variable "cloud_id" {
+  type    = string
+  default = "b1goa359kc9fcau5aga3"
+}
+
+variable "folder_id" {
+  type    = string
+  default = "b1gt4mauq7s0gcln199b"
+}
+
+variable "zone" {
+  type    = string
+  default = "ru-central1-c"
+}
+
+
